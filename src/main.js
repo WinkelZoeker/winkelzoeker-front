475189c0a59a8d5a4ae08f520d557147c3278601
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-21 13:05:17 +0000

    Plugins added

--- src/main.js
+++ src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: (h) => {
+    return(
+      <header>
+        <h1>Titulo do Post</h1>  
+        <p>Conteudo do Post</p>
+      </header>
+    )}
 })
