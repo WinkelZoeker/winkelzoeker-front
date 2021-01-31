6e433b619d1ca40fbf02695453d07e10e4f2815f
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-31 18:47:41 +0100

    Validation for error

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app" class="small-container">
-    <h1>WinkelZoeker - Version 1</h1>
-
-    <search-form @search:store="searchStore" />
+    <h1>WinkelZoeker</h1>
+    <search-form @search:store="searchStore" :error_api="error_api" :invalidLongitude="invalidLongitude" :invalidLatitude="invalidLatitude" :invalidLimit="this.invalidLimit" />
     <stores-table :stores="stores" />
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 import SearchForm from "@/components/SearchForm.vue";
 import StoresTable from "@/components/StoresTable.vue";
 
@@ -20,24 +19,44 @@ export default {
   },
   data() {
     return {
+      error_api: "",
       stores: [],
+      invalidLongitude: false,
+      invalidLatitude: false,
+      invalidLimit: false
     };
   },
   methods: {
+    clearStatus() {
+      this.invalidLongitude = false;
+      this.invalidLatitude = false;
+      this.invalidLimit = false;
+      this.error_api = "";
+    },
+
     searchStore(searchParams) {
+      this.clearStatus();
       const searchApiUrl = process.env.VUE_APP_SEARCH_API_URL;
       const queryParams = {
-        params: {...searchParams}
+        params: { ...searchParams },
       };
 
-      axios.get(searchApiUrl, queryParams).then(response => {
-        console.log(`RESPONSE: ${JSON.stringify(response, null, 2)}`);
-        this.stores = response.data.data;
-      }).catch(error => {
-        console.log(`ERROR: ${JSON.stringify(error, null, 2)}`);
-      });
+      axios
+        .get(searchApiUrl, queryParams)
+        .then((response) => {
+          this.stores = response.data.data;
+        })
+        .catch((error) => {
+          this.stores = [];
+          this.error_api = error.response.data.error;
 
-},
+          console.log(`error.response.data => ${JSON.stringify(error.response.data, null, 2)}`);
+          this.invalidLongitude = error.response.data.error.includes('longitude');
+          this.invalidLatitude =  error.response.data.error.includes('latitude');
+          this.invalidLimit =  error.response.data.error.includes('limit');
+
+        });
+    },
   },
 };
 </script>
--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -2,19 +2,30 @@
   <div id="search-form">
     <form @submit.prevent="handleSubmit">
       <label>Latitude</label>
-      <input v-model="searchParams.latitude" type="text" />
+      <input v-model="searchParams.latitude" type="text" :class="{ 'has-error': invalidLatitude }"/>
       <label>Longitude</label>
-      <input v-model="searchParams.longitude" type="text" />
+      <input v-model="searchParams.longitude" type="text" :class="{ 'has-error': invalidLongitude }"/>
       <label>Limit</label>
-      <input v-model="searchParams.limit" type="text" />
+      <input v-model="searchParams.limit" type="text" :class="{ 'has-error': invalidLimit }"/>
+
+      <p v-if="error_api" class="error-message">{{error_api}}</p>
+
       <button>Search</button>
     </form>
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: "search-form",
+  props: {
+    error_api: String,
+    invalidLongitude: Boolean,
+    invalidLatitude: Boolean,
+    invalidLimit: Boolean
+  },
   data() {
     return {
       searchParams: {
@@ -35,5 +46,16 @@ export default {
 <style scoped>
 form {
   margin-bottom: 2rem;
+}
+[class*="-message"] {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
 }
 </style>
